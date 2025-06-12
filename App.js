57578712324e27175b988d0b52f25d7fa531ff3d
Josh <jsbusiness305@gmail.com>
2025-06-12 19:02:35 -0400

    added a Modal for goal input

--- App.js
+++ App.js
@@ -1,31 +1,53 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Modal, Button } from "react-native";
 import { useState } from "react";
 import GoalInput from "./components/GoalInput";
 import uuid from "react-native-uuid";
 import GoalList from "./components/GoalList";
 export default function App() {
   const [goals, setGoals] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   function addGoalHandler(goalInput) {
     setGoals((prev) => [
       ...prev,
       { key: uuid.v4().toString(), text: goalInput },
     ]);
+
+    //close modal after adding goal
+    setIsModalVisible(false);
   }
 
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <GoalInput onGoalAdd={(goalInput) => addGoalHandler(goalInput)} />
+    <>
+      {isModalVisible && (
+        <Modal
+          visible={isModalVisible}
+          animationType="slide"
+          onRequestClose={() => setIsModalVisible(false)}
+        >
+          <View style={styles.addGoalModal}>
+            <View style={styles.inputContainer}>
+              <GoalInput onGoalAdd={(goalInput) => addGoalHandler(goalInput)} />
+            </View>
+          </View>
+        </Modal>
+      )}
+      <View style={styles.appContainer}>
+        <View>
+          <Button
+            title="Add New Goal"
+            onPress={() => setIsModalVisible(true)}
+          />
+        </View>
+        <GoalList
+          dataList={goals}
+          allowToEdit
+          onDelete={(id) =>
+            setGoals((prev) => prev.filter((goal) => goal.key !== id))
+          }
+        />
       </View>
-      <GoalList
-        dataList={goals}
-        allowToEdit
-        onDelete={(id) =>
-          setGoals((prev) => prev.filter((goal) => goal.key !== id))
-        }
-      />
-    </View>
+    </>
   );
 }
 
@@ -39,5 +61,12 @@ const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
+  },
+  addGoalModal: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
   },
 });
